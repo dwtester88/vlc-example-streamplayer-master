47df868ac8427b331cee0cf2c8ec86e76dd107ac
dwtester88 <31963767+dwtester88@users.noreply.github.com>
2017-09-28 14:39:14 -0400

    no change

--- app/src/main/java/com/pedro/vlctestapp/BootCompletedIntentReceiver.java
+++ app/src/main/java/com/pedro/vlctestapp/BootCompletedIntentReceiver.java
@@ -10,13 +10,13 @@ import android.content.Intent;
 
 // This class is implemented to start the Service listner when device is booted.
 public class BootCompletedIntentReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-            if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-                Intent pushIntent = new Intent(context, ServiceListner.class);
-                context.startService(pushIntent);
-            }
+@Override
+public void onReceive(Context context, Intent intent) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+        Intent pushIntent = new Intent(context, ServiceListner.class);
+        context.startService(pushIntent);
+        }
 
 
-    }
-}
+        }
+        }
